ed4f127a38ec3df6f7884a0bc7b6202822fc3964
airflowboy <ajtwoddl9194@gmail.com>
2024-12-08 22:38:08 +0900

    MSA Pay Starter Template

---
+++ src/main/java/com/project/myfastcampuspay/MyFastCampusPayApplication.java
@@ -0,0 +1,13 @@
+package com.project.myfastcampuspay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFastCampusPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFastCampusPayApplication.class, args);
+    }
+
+}
